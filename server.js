fbbaf16c388cc552acc27199960788109a6a3210
tejaswigowda <tejaswi@foxyninjastudios.com>
2021-02-09 12:31:54 -0700

    ac

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-// http://127.0.0.1:9001
-// http://localhost:9001
+// Muaz Khan      - www.MuazKhan.com
+// MIT License    - www.WebRTC-Experiment.com/licence
+// Documentation  - github.com/muaz-khan/getScreenId
+
+var port = process.env.PORT || 9001;
 
 var server = require('http'),
     url = require('url'),
@@ -7,68 +10,76 @@ var server = require('http'),
     fs = require('fs');
 
 function serverHandler(request, response) {
-    var uri = url.parse(request.url).pathname,
-        filename = path.join(process.cwd(), uri);
+    try {
+        var uri = url.parse(request.url).pathname,
+            filename = path.join(process.cwd(), uri);
 
-    fs.exists(filename, function(exists) {
-        if (!exists) {
+        if (filename && filename.search(/server.js/g) !== -1) {
             response.writeHead(404, {
                 'Content-Type': 'text/plain'
             });
-            response.write('404 Not Found: ' + filename + '\n');
+            response.write('404 Not Found: ' + path.join('/', uri) + '\n');
             response.end();
             return;
         }
 
-        if (filename.indexOf('favicon.ico') !== -1) {
+        var stats;
+
+        try {
+            stats = fs.lstatSync(filename);
+        } catch (e) {
+            response.writeHead(404, {
+                'Content-Type': 'text/plain'
+            });
+            response.write('404 Not Found: ' + path.join('/', uri) + '\n');
+            response.end();
             return;
         }
 
-        var isWin = !!process.platform.match(/^win/);
+        if (fs.statSync(filename).isDirectory()) {
+            response.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
 
-        if (fs.statSync(filename).isDirectory() && !isWin) {
             filename += '/index.html';
-        } else if (fs.statSync(filename).isDirectory() && !!isWin) {
-            filename += '\\index.html';
         }
 
-        fs.readFile(filename, 'binary', function(err, file) {
+
+        fs.readFile(filename, 'utf8', function(err, file) {
             if (err) {
                 response.writeHead(500, {
                     'Content-Type': 'text/plain'
                 });
-                response.write(err + '\n');
+                response.write('404 Not Found: ' + path.join('/', uri) + '\n');
                 response.end();
                 return;
             }
 
-            var contentType;
-
-            if (filename.indexOf('.html') !== -1) {
-                contentType = 'text/html';
-            }
-
-            if (filename.indexOf('.js') !== -1) {
-                contentType = 'application/javascript';
-            }
-
-            if (contentType) {
-                response.writeHead(200, {
-                    'Content-Type': contentType
-                });
-            } else response.writeHead(200);
-
-            response.write(file, 'binary');
+            response.writeHead(200);
+            response.write(file, 'utf8');
             response.end();
         });
+    } catch (e) {
+        response.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        response.write('<h1>Unexpected error:</h1><br><br>' + e.stack || e.message || JSON.stringify(e));
+        response.end();
+    }
+}
+
+var app = server.createServer(serverHandler);
+
+function runServer() {
+    app = app.listen(port, process.env.IP || '0.0.0.0', function() {
+        var addr = app.address();
+
+        if (addr.address === '0.0.0.0') {
+            addr.address = 'localhost';
+        }
+
+        console.log('Server listening at http://' + addr.address + ':' + addr.port);
     });
 }
 
-var app;
-
-app = server.createServer(serverHandler);
-
-app = app.listen(process.env.PORT || 9001, process.env.IP || "0.0.0.0", function() {
-    var addr = app.address();
-    console.log("Server listening at", addr.address + ":" + addr.port);
-});
+runServer();
